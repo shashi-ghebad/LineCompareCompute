29bf030c0a151e6feeb6096786eb2548ad5be00b
Shashi <shashihghebad3@gmail.com>
2021-03-03 21:45:29 -0800

    [SHASHI]Add:TwoLinesEquality

---
+++ LineComp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
