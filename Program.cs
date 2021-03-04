35d86be59ffe6ac637d1819e7e4ec60b846c4f6a
Shashi <shashihghebad3@gmail.com>
2021-03-04 09:58:13 -0800

    [SHASHI]Add:UC3-TwoLinesCompare

--- Program.cs
+++ Program.cs
@@ -1,21 +1,53 @@
 ﻿using System;
 
-namespace LineCompare
+namespace TwoLinesCompare
 {
     class Program
     {
-       
-        // Function to calculate 
-        static double distance(int x1, int y1, int x2, int y2)
+        static void Main(string[] args)
         {
-            // Calculating distance 
-                return Math.Sqrt(Math.Pow(x2 - x1, 2) +  Math.Pow(y2 - y1, 2) * 1.0);
-        }
+            Console.WriteLine("Enter the first x1 co-ordinate point: ");
+            double x1 = (double)Convert.ToDecimal(Console.ReadLine());
 
-                public static void Main()
-                {
-                Console.WriteLine(Math.Round(distance(5, 6, 6, 5) * 100.0) / 100.0);
+            Console.WriteLine("Enter the first y1 co-ordinate point: ");
+            double y1 = (double)Convert.ToDecimal(Console.ReadLine());
 
+            Console.WriteLine("Enter the first x2 co-ordinate point: ");
+            double x2 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first y2 co-ordinate point: ");
+            double y2 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            double length1 = (double)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+
+            Console.WriteLine("Enter the first a1 co-ordinate point: ");
+            double a1 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first b1 co-ordinate point: ");
+            double b1 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first a2 co-ordinate point: ");
+            double a2 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter the first b2 co-ordinate point: ");
+            double b2 = (double)Convert.ToDecimal(Console.ReadLine());
+
+            double length2 = (double)Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+
+
+            Console.WriteLine("Length of the first line is: " + length1);
+            Console.WriteLine("Length of the second line is: " + length2);
+
+
+            if (length1 > length2)
+                Console.WriteLine("length1 is Greater");
+            else if (length1 < length2)
+                Console.WriteLine("length1 is Smaller");
+            else
+                Console.WriteLine("Lines are Equal ");
+
+            Console.ReadKey();
         }
     }
 }
